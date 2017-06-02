9a77753a46a0d006b0fd24096d3c9c7c74f4e09e
KunlinY <KunlinY@qq.com>
2017-06-03 03:50:07 +0800

    model建立完成

--- WorkshopDBSystem/settings.py
+++ WorkshopDBSystem/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Workshop',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,13 @@ WSGI_APPLICATION = 'WorkshopDBSystem.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'workshop',
+        'USER': 'root',
+        'PASSWORD': '1028',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
